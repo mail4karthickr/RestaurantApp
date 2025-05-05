1d70b17573bc85b0b8e167742e6f5331c4bc32b4
Karthick <mail4karthickr@gmail.com>
2025-05-05 17:14:01 +0530

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Karthick Ramasamy on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
