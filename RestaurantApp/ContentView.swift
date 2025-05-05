d2db3e3bac280b6ced861c466ac52675b071f18e
Karthick <mail4karthickr@gmail.com>
2025-05-05 17:53:59 +0530

    updated

--- RestaurantApp/ContentView.swift
+++ RestaurantApp/ContentView.swift
@@ -6,14 +6,14 @@
 //
 
 import SwiftUI
+import RestaurantFinder
+import RestaurantDetails
+import Checkout
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RestaurantFinderView()
         }
         .padding()
     }
